01a125956a787f19372492232483da7685a39eb1
1abme <124783124+1abme@users.noreply.github.com>
2024-04-02 15:36:52 +0900

    [FIX]금액변경히스토리 스타일 수정

--- src/components/common/Order/AmountChangeHistory.tsx
+++ src/components/common/Order/AmountChangeHistory.tsx
@@ -16,7 +16,7 @@ const AmountChangeHistory = ({
       />
       {changeHistory?.map((info) => (
         <div
-          className="mt-[30px] border-y-[2px] border-sub-black"
+          className="mt-[30px] border-y-[2px] border-sub-black flex flex-col"
           key={info.updateDate}
         >
           <div className="flex">
@@ -24,33 +24,37 @@ const AmountChangeHistory = ({
               category={`변경일시`}
               header={false}
               content={info.updateDate}
+              rowStyle="w-[33%] min-w-fit"
             />
             <TableRow
               category="추가/감소 금액"
               header={false}
               content={`${amountFormat(info.changedPrice)} 원`}
+              rowStyle="w-[33%] min-w-fit"
             />
             <TableRow
               category="결제/취소 금액"
               content={`${amountFormat(info.payedPrice)} 원`}
               header={false}
+              rowStyle="w-[33%] min-w-fit"
             />
           </div>
           <div className="border-y border-sub-black">
             <TableRow category="변경사유" header={false} content={info.memo} />
           </div>
-          <div className="flex justify-end w-full">
+          <div className="flex">
+            <div className="w-[33%]"></div>
             <TableRow
               category="변경후 총액"
               header={false}
               content={`${amountFormat(info.totalPriceSnapshot)} 원`}
-              rowStyle={"w-[33%]"}
+              rowStyle={"w-[33%] min-w-fit"}
             />
             <TableRow
               category="변경후 잔액"
               header={false}
               content={`${amountFormat(info.balanceSnapshot)} 원`}
-              rowStyle={"w-[33%]"}
+              rowStyle={"w-[33%] min-w-fit"}
             />
           </div>
         </div>
