14b2c678e917dda8d78439d2cfaa2f62c39f0477
leejh4197 <leejh4197@naver.com>
2024-04-03 21:25:35 +0900

    [STYLE]모바일 반응형

--- src/components/common/MbNav.tsx
+++ src/components/common/MbNav.tsx
@@ -1,6 +1,6 @@
 const MbNav = () => {
   return (
-    <div className="z-50 fixed bottom-0 w-full h-[75px] hidden max-xsm:block bg-red-500">
+    <div className="z-50 fixed bottom-0 w-full h-[75px] hidden bg-red-500 max-xsm:block ">
       네비게이션
     </div>
   );
